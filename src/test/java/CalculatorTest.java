8f8c7a730a1ba7d8ea65fd4a473a336386f268c7
Davidfcg20 <david.catano@correo.tdea.edu.co>
2025-08-20 17:16:35 -0500

    New: Added new assertions methods to CalculatorTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,27 @@
 import org.example.Calculator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
-    public void sum(){
+    public void assertionsMethodsEqual(){
         assertEquals(3.3, calculator.sum(1.2, 2.1));
+        assertNotEquals(4.1, calculator.multiply(2.0, 2.0));
+    }
+
+    @Test
+    public void assertionsMethodsPrimitive(){
+        Double result = calculator.divide(10.0, 2.0);
+        assertTrue(result == 5.0);
+    }
+
+    @Test
+    public void assertionsMethodsRef(){
+        Double result = calculator.subtract(2.0, 1.0);
+        assertNull(result == 3.0);
     }
 }
